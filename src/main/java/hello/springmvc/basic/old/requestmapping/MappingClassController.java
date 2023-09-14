919a44c3ec2cce35e791ace34463a8f0751c8170
bcjeong <sada1223@b2en.com>
2023-09-14 14:05:34 +0900

    복습

--- src/main/java/hello/springmvc/basic/old/requestmapping/MappingClassController.java
+++ src/main/java/hello/springmvc/basic/old/requestmapping/MappingClassController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/mapping/users")
 public class MappingClassController {
 
+    // 전송 방식에 따라서 같은 url 이지만 요청으로 기능 구분 가능
 
     @GetMapping
     public String user(){
